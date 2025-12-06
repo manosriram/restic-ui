6f91ae01b590b50b65a2bdb97ef2bbc2b56760c4
Mano Sriram <mano.sriram0@gmail.com>
2025-12-06 21:55:43 +0530

    feat: integrate ResticUI snapshots and set app host to 100.69.69.69

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+from restic import ResticUI
+
 app = Flask(__name__)
 
 @app.route("/")
 def default_route():
-    return "Hello, World!"
+    return ResticUI().get_snapshots()
+    #  return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="100.69.69.69")
